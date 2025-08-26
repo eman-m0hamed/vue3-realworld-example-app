0cf9dbe23ad665d053b7af8d2d9e3cb4cfe84b18
eman-m0hamed <emanmohamed73359@gmail.com>
2025-08-26 03:05:42 +0300

    at ArticleDetail component set button to show article revision in case of user is authorized at a section in the same page

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -7,6 +7,7 @@
         v-if="article"
         :article="article"
         @update="updateArticle"
+        @showRevisions="onShowRevisions"
       />
     </div>
   </div>
@@ -17,6 +18,9 @@
       <div id="article-content" class="col-md-12" v-html="articleHandledBody" />
       <!-- eslint-enable vue/no-v-html -->
 
+      <div v-if="revertMsg" :class="['alert', revertMsgType === 'success' ? 'alert-success' : 'alert-danger']" style="margin: 1em 0;">
+        {{ revertMsg }}
+      </div>
       <!-- TODO: abstract tag list component -->
       <ul class="tag-list">
         <li
@@ -29,33 +33,102 @@
       </ul>
     </div>
 
-    <hr>
+
 
     <div class="article-actions">
       <ArticleDetailMeta
         v-if="article"
         :article="article"
         @update="updateArticle"
+        @showRevisions="onShowRevisions"
       />
+      <hr>
+
+       <!-- Article Revisions List -->
+    <ArticleDetailRevisionsList
+      v-if="showRevisionsList"
+      :revisions="revisions"
+      @revert="revertToRevision"
+    />
+
+    <hr>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive } from 'vue'
+import Swal from 'sweetalert2'
+import { computed, reactive, ref } from 'vue'
+  const revertMsg = ref('')
+  const revertMsgType = ref<'success' | 'error'>('success')
 import { useRoute } from 'vue-router'
 import marked from 'src/plugins/marked'
 import { api } from 'src/services'
 import type { Article } from 'src/services/api'
 import ArticleDetailMeta from './ArticleDetailMeta.vue'
+import ArticleDetailRevisionsList from './ArticleDetailRevisionsList.vue'
+import type { ArticleRevision } from 'src/services/api'
 
 const route = useRoute()
 const slug = route.params.slug as string
 const article: Article = reactive(await api.articles.getArticle(slug).then(res => res.data.article))
+const showRevisionsList = ref(false)
+const revisions = ref<ArticleRevision[]>([])
 
 const articleHandledBody = computed(() => marked(article.body))
 
+
 function updateArticle(newArticle: Article) {
   Object.assign(article, newArticle)
+}
+
+async function onShowRevisions() {
+  // Always use the current article.slug for API calls
+  const res = await api.articles.getArticleRevisions(article.slug)
+  revisions.value = res.data.data || []
+  showRevisionsList.value = true
+}
+
+
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+async function revertToRevision(revisionId: number) {
+  try {
+    // Always use the current article.slug for API calls
+    let revetRes = await api.articles.revertArticleToRevision(article.slug, revisionId)
+    let newSlug = revetRes?.data?.data?.slug;
+    await Swal.fire({
+      icon: 'success',
+      title: 'Success',
+      text: 'Article successfully reverted to the selected revision.',
+      timer: 2500,
+      showConfirmButton: false
+    })
+    if (!newSlug) {
+      return;
+    }
+
+    // Change slug in the URL
+    router.push({ name: "article", params: { slug: newSlug } });
+    // Refresh the article data
+    const res = await api.articles.getArticle(newSlug)
+    Object.assign(article, res.data.article)
+
+    // Refresh the revisions list
+    const revRes = await api.articles.getArticleRevisions(newSlug)
+    revisions.value = revRes.data.data || []
+    showRevisionsList.value = true
+  } catch (err: any) {
+    const errorMsg = err?.error?.body?.[0] || err?.message || 'Failed to revert article.'
+    await Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: errorMsg,
+      timer: 2000,
+      showConfirmButton: false
+    })
+  }
 }
 </script>
